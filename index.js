1cabe5e9033b8a704f6bb66bd3cd36795e9092e0
Setherizor <setherizor@gmail.com>
2018-06-13 15:19:45 -0400

    added urls for safe keeping

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const chalk = require('chalk') // https://www.npmjs.com/package/chalk
 const program = require('commander') // https://www.npmjs.com/package/commander
 const commmands = require('./lib/commands')
 
+
+
+// https://www.npmjs.com/package/cliui
+// https://www.npmjs.com/package/last-line
+// https://www.npmjs.com/package/read-last-lines
+// https://www.npmjs.com/package/node-run-cmd
+// https://www.npmjs.com/package/commander
+// https://www.npmjs.com/package/chalk
+// https://www.npmjs.com/package/configstore
+
 program
   .usage('[command name] OR [options] ')
   .version(require('./package.json').version)
